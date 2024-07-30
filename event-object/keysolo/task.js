f4043b40a84c8b9eb5f4d064f06c3d3f1f2df7a3
fdooch <amigoist@yandex.ru>
2024-07-30 11:33:54 +0300

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -25,7 +25,14 @@ class Game {
       При неправильном вводе символа - this.fail();
       DOM-элемент текущего символа находится в свойстве this.currentSymbol.
      */
-  }
+      document.addEventListener("keyup", (event) => {
+        if (event.key.toLowerCase() == this.currentSymbol.textContent.toLowerCase()) {
+          this.success();
+        } else {
+          this.fail();
+        }
+      });
+    }
 
   success() {
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
